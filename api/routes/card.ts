0dc206082221467532a228113dc5d3b1674c59c8
David <78974474+dedestem@users.noreply.github.com>
2025-09-22 08:24:26 +0200

    Update card.ts

--- api/routes/card.ts
+++ api/routes/card.ts
@@ -1,5 +1,5 @@
 import { Router } from "https://deno.land/x/oak@v12.6.1/mod.ts";
-import { add_card, change_card_title, create_checklist_item, delete_checklist_item, move_card, update_card_description } from "../controllers/cards.ts";
+import { add_card, change_card_title, create_checklist_item, delete_checklist_item, move_card, update_card_description, delete_card} from "../controllers/cards.ts";
 import auth from "../middlewares/auth.ts";
 
 
@@ -8,6 +8,7 @@ const router = new Router();
 router
     .post("/add", auth, add_card)
     .post("/move", auth, move_card)
+    .post("/delete", auth delete_card)
     .post("/change-title", auth, change_card_title)
     .post("/create-checklist", auth, create_checklist_item)
     .post("/delete-checklist", auth, delete_checklist_item)
