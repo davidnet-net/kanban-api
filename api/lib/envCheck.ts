6cd5e24054888cb1a012dc7a79f08acdd9a3635a
dedestem <david@davidnet.net>
2025-11-08 19:01:45 +0100

    Add RabbitMQ (Secrets)

--- api/lib/envCheck.ts
+++ api/lib/envCheck.ts
@@ -10,6 +10,12 @@ export function envCheck() {
 	if (!DA_DB_PASS || !typeof String) return "DA_DB_PASS";
 	if (!DB_NAME || !typeof String) return "DA_DB_NAME";
 
+	//? RabbitMQ
+	const RABBITMQ_USER = Deno.env.get("DA_RABBITMQ_USER");
+	const RABBITMQ_PASS = Deno.env.get("DA_RABBITMQ_PASS");
+	if (!RABBITMQ_USER || !typeof String ){ return "DA_RABBITMQ_USER"; }
+	if (!RABBITMQ_PASS || !typeof String ){ return "DA_RABBITMQ_PASS"; }
+
 	//? Logging
 	const DA_LOG_DIR = Deno.env.get("DA_LOG_DIR");
 	const DA_KEEP_LOG_DAYS = Deno.env.get("DA_KEEP_LOG_DAYS");
